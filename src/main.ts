9e80919e413756544655e15384be8d50eb62ee7b
Kai <Haunv1503@gmail.com>
2024-02-26 00:19:04 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,125 +1,125 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
-import * as session from 'express-session';
-import * as cookieParser from 'cookie-parser';
-import helmet from 'helmet';
-import * as csurf from 'csurf';
-import { AllExceptionsFilter } from './configure/security/exception/http-exception.filter';
-import hbs, { handlebars } from 'hbs';
-import { readFileSync } from 'fs';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as moment from 'moment';
-import { I18nContext } from 'nestjs-i18n';
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
+import * as session from "express-session";
+import * as cookieParser from "cookie-parser";
+import helmet from "helmet";
+import * as csurf from "csurf";
+import { AllExceptionsFilter } from "./configure/security/exception/http-exception.filter";
+import hbs, { handlebars } from "hbs";
+import { readFileSync } from "fs";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as moment from "moment";
+import { I18nContext } from "nestjs-i18n";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix(process.env.PREFIX_URL);
   app.enableCors();
   //  configure view engine and static assets
-  
-  app.useStaticAssets(process.cwd() + '/assets/public');
-  app.setBaseViewsDir(process.cwd() + '/assets/views');
+
+  app.useStaticAssets(process.cwd() + "/assets/public");
+  app.setBaseViewsDir(process.cwd() + "/assets/views");
 
   handlebars.registerPartial(
-    'head_component',
+    "head_component",
     readFileSync(
       join(
         __dirname,
-        '..',
-        'assets/views/partials/head_component.hbs',
+        "..",
+        "assets/views/partials/head_component.hbs"
       ).toString(),
-      'utf8',
-    ),
+      "utf8"
+    )
   );
 
   handlebars.registerPartial(
-    'header_component',
+    "header_component",
     readFileSync(
       join(
         __dirname,
-        '..',
-        'assets/views/partials/header_component.hbs',
+        "..",
+        "assets/views/partials/header_component.hbs"
       ).toString(),
-      'utf8',
-    ),
+      "utf8"
+    )
   );
 
   handlebars.registerPartial(
-    'footer_component',
+    "footer_component",
     readFileSync(
       join(
         __dirname,
-        '..',
-        'assets/views/partials/footer_component.hbs',
+        "..",
+        "assets/views/partials/footer_component.hbs"
       ).toString(),
-      'utf8',
-    ),
+      "utf8"
+    )
   );
 
   handlebars.registerHelper("inc", function (value, options) {
     return parseInt(value) + 1;
   });
   handlebars.registerHelper("ifNot", function (value, options) {
-    return (value == false) ? options.fn(this) : options.inverse(this);
+    return value == false ? options.fn(this) : options.inverse(this);
   });
-  handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+  handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
     console.log(arg1, arg2, arg1 == arg2);
 
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    return arg1 == arg2 ? options.fn(this) : options.inverse(this);
   });
-  handlebars.registerHelper('ifNotEquals', function (arg1, arg2, options) {
+  handlebars.registerHelper("ifNotEquals", function (arg1, arg2, options) {
     console.log(arg1, arg2);
-    return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+    return arg1 != arg2 ? options.fn(this) : options.inverse(this);
   });
 
-  handlebars.registerHelper('recentDate', function (value, options) {
+  handlebars.registerHelper("recentDate", function (value, options) {
     return moment(value).fromNow();
   });
 
-  handlebars.registerHelper('vnDateFormat', function (value, options) {
+  handlebars.registerHelper("vnDateFormat", function (value, options) {
     return moment(value).format("DD/MM/yyyy");
   });
 
-  handlebars.registerHelper('classSessionDay', function (value: string, options) {
-    let days: string[]= value.split(',').map(d => 'T'+ (Number(d.replace('day', '')) + 1));
-    return days.join('-');
-  });
-  handlebars.registerHelper('classSessionTime', function (value, options) {
-    return 'Từ ' + value.split(",")[0] + "h" + '-' + value.split(",")[1] + "h"
+  handlebars.registerHelper(
+    "classSessionDay",
+    function (value: string, options) {
+      let days: string[] = value
+        .split(",")
+        .map((d) => "T" + (Number(d.replace("day", "")) + 1));
+      return days.join("-");
+    }
+  );
+  handlebars.registerHelper("classSessionTime", function (value, options) {
+    return "Từ " + value.split(",")[0] + "h" + "-" + value.split(",")[1] + "h";
   });
 
-  handlebars.registerHelper('dateTimeFormat', function (value, options) {
+  handlebars.registerHelper("dateTimeFormat", function (value, options) {
     return moment(value).format("yyyy-MM-dd HH:mm:ss");
   });
 
-  handlebars.registerHelper('prefixLocaleHref', function (value, options) {
-    return I18nContext.current().lang == "vi"  ? "" : `/${I18nContext.current().lang}`;
+  handlebars.registerHelper("prefixLocaleHref", function (value, options) {
+    return I18nContext.current().lang == "vi"
+      ? ""
+      : `/${I18nContext.current().lang}`;
   });
 
-  handlebars.registerHelper('currentLang', function (value, options) {
-    return I18nContext.current().lang ;
+  handlebars.registerHelper("currentLang", function (value, options) {
+    return I18nContext.current().lang;
   });
 
-  
-  
-  
-  moment.locale('vi');
+  moment.locale("vi");
 
-  app.setViewEngine('hbs');
+  app.setViewEngine("hbs");
 
-
-
-
-  
   //  configure session
   app.use(
     session({
-      secret: 'jshikisession',
+      secret: "jshikisession",
       resave: false,
       saveUninitialized: true,
-    }),
+    })
   );
 
   // configure cookie parser
@@ -135,22 +135,21 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
+  if (process.env.NODE_ENV != "production") {
+    // config swagger
+    const config = new DocumentBuilder()
+      .setTitle("Korean Education center example")
+      .setDescription("The Korean Education center API description")
+      .setVersion("1.0")
+      .addApiKey({ type: "apiKey", name: "x-access-token", in: "header" })
+      .setBasePath("api")
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api/docs", app, document);
+  }
 
-  // config swagger
-  const config = new DocumentBuilder()
-    .setTitle('Korean Education center example')
-    .setDescription('The Korean Education center API description')
-    .setVersion('1.0')
-    .addApiKey({ type: 'apiKey', name: 'x-access-token', in: 'header' })
-    .setBasePath('api')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
-  
-  await app.listen(process.env.PORT);
-
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
-
